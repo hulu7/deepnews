56222e7635a370e706b93545a520c7d0b38104dc
dev <gerrit_user1@sina.com>
2019-11-18 20:41:14 +0800

    update the style
    Change-Id: Idc039ac01600a082c729780f59d102f32fe9a226

--- public/src/app/const/common-variables.ts
+++ public/src/app/const/common-variables.ts
@@ -1,3 +1,3 @@
 // export const currentDateUrl = 'http://localhost:3000/api/currentDate';
 
-export const currentDateUrl = 'http://www.deepinews.com:3000/api/currentDate';
+export const currentDateUrl = 'http://www.deepinews.com:10003/api/currentDate';
--- public/src/app/servcies/listContentService.ts
+++ public/src/app/servcies/listContentService.ts
@@ -8,6 +8,6 @@ export class ListContentService {
 
     public getArticles(page: any, pageLimit: any, catalog: string): Observable<any> {
         // return this.http.get(`http://localhost:3000/api/article/getArticle?page=${page}&limit=${pageLimit}&catalog=${catalog}`);
-        return this.http.get(`http://www.deepinews.com:3000/api/article/getArticle?page=${page}&limit=${pageLimit}&catalog=${catalog}`);
+        return this.http.get(`http://www.deepinews.com:10003/api/article/getArticle?page=${page}&limit=${pageLimit}&catalog=${catalog}`);
     }
 }
