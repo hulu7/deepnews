4b793b2040438309db7d57773f279524621ebf0d
dev <gerrit_user1@sina.com>
2020-03-28 11:03:33 +0800

    add loader and update string
    Change-Id: I94b99dd939d3288d964fd110967a32ae7746764e

--- public/src/app/components/header/header.component.ts
+++ public/src/app/components/header/header.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
-import { TimeService } from '../../servcies/timeService';
 import { DatePipe } from '@angular/common';
 import { PageService } from "../../servcies/pageService";
 import { takeUntil } from "rxjs/operators";
@@ -21,8 +20,6 @@ export class HeaderComponent implements OnInit, OnDestroy {
   public date: string;
 
   constructor(private router: Router,
-              private timeService: TimeService,
-              private datePipe: DatePipe,
               private pageService: PageService) {
     this.pageService.isHome$
         .pipe(takeUntil(this.destroy$))
@@ -35,22 +32,11 @@ export class HeaderComponent implements OnInit, OnDestroy {
     if (window.location.pathname !== '/') {
       this.isHomePage = false;
     }
-    this.initDate();
   }
 
   ngOnDestroy(): void {
     this.destroy$.next(true);
     this.destroy$.unsubscribe();
-  }
-
-  private initDate(): void {
-    this.timeService.getCurrentDate()
-        .pipe(takeUntil(this.destroy$))
-        .subscribe(data => {
-      let result = JSON.parse(data._body);
-      let yyMmDdArray = this.datePipe.transform(result.date,'yyyy-MM-dd').split('-');
-      this.date = yyMmDdArray[0] + '年' + yyMmDdArray[1] + '月' + yyMmDdArray[2] + '日'
-    })
   }
 
   public goToComplainPage(): void {
